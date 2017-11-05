9d206321c2baab93b11c2b69a4e16b9d8f71d27a
Nestor Alvarez Diaz <nestor@consensusx.com>
2017-11-05 12:17:26 +0000

    Express basic https and http redirection

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require('path');
+const bodyParser = require("body-parser");
+const pug = require('pug');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+/**
+ * Configure HTTPS thanks to use Let's Encrypt
+ */
+
+const credentials = {
+  key: fs.readFileSync('/etc/letsencrypt/live/cv.nalvared.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/cv.nalvared.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/cv.nalvared.com/chain.pem')
+};
+
+const app = express();
+
+/**
+ * Configure server
+ */
+app.use(express.static(__dirname + '/public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+/**
+ * Launching
+ */
+var httpServer = http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+});
+
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, () => {
+  console.log('Application is running in non-safe mode over the port 8080');
+  console.log('All traffic will be redirect to https server');
+});
+
+httpsServer.listen(8443, () => {
+  console.log('Application is running in safe mode over the port 8443');
+});
